4d70b8ffb313d9828c6f622fc3692b8011fb6e1e
Ben Radley <bdr1224@rit.edu>
2021-04-17 14:38:12 -0400

    Updated Advisor home with own calendar, remove student studentsen heading in future commit

--- src/components/AdvisorHome.js
+++ src/components/AdvisorHome.js
@@ -10,13 +10,13 @@ function AdvisorHome() {
             <div className='calendar-monthly'>
                 <h2>Calendar</h2>
                 <iframe className='monthly' title='monthly'
-                        src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FNew_York&amp;src=c3dlbjQ0NGZha2VzdHVkZW50QGdtYWlsLmNvbQ&amp;color=%23039BE5&amp;showTz=0&amp;showCalendars=0&amp;showTabs=0&amp;showPrint=0&amp;showTitle=0&amp;mode=MONTH&amp;showNav=0&amp;showDate=0"
+                        src="https://calendar.google.com/calendar/embed?src=rittestadvisor%40gmail.com&ctz=America%2FNew_York" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"
                         style={style}/>
             </div>
             <div className='calendar-agenda'>
                 <h2>Agenda</h2>
                 <iframe className='agenda' title='agenda'
-                        src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FNew_York&amp;src=c3dlbjQ0NGZha2VzdHVkZW50QGdtYWlsLmNvbQ&amp;color=%23039BE5&amp;showTz=0&amp;showCalendars=0&amp;showTabs=0&amp;showPrint=0&amp;showTitle=0&amp;mode=AGENDA&amp;showNav=0&amp;showDate=0"
+                        src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FNew_York&amp;src=rittestadvisor%40gmail.com&amp;color=%23039BE5&amp;showTz=0&amp;showCalendars=0&amp;showTabs=0&amp;showPrint=0&amp;showTitle=0&amp;mode=AGENDA&amp;showNav=0&amp;showDate=0"
                         style={style} />
             </div>
         </div>
