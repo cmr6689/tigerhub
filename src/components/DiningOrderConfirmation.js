b1a98b3cd962828c6e53d7dea724dc2768bea3ca
Seth Teichman <smt5541@rit.edu>
2021-04-17 21:17:30 -0400

    Fix bug in Dining Order Confirmation, reinstall gh-pages

--- src/components/DiningOrderConfirmation.js
+++ src/components/DiningOrderConfirmation.js
@@ -12,7 +12,7 @@ class DiningOrderCheckout extends Component {
                         <h1>Order Confirmation</h1><br/>
                         <h1>5541</h1><br/><br/><br/>
                         <span>Your order has been sent to <b>{this.props.restaurant}</b> for pickup between <b>{this.props.time}</b>.</span><br/><br/>
-                        <span>Your total is <b>${this.props.calculateTotal()}</b>, charged to <b>{this.props.paymentMethod}</b>.</span><br/><br/>
+                        <span>Your total is <b>${this.props.calculateTotal().toFixed(2)}</b>, charged to <b>{this.props.paymentMethod}</b>.</span><br/><br/>
                         <span>Once it is time to begin preparing your order, you will receive a text message on your phone. You will receive another message once your order is ready for pickup.</span><br/><br/>
                         <span>Please do not enter the restaurant until your order is ready.</span><br/>
                         <span>Thanks!</span><br/><br/><br/><br/>
