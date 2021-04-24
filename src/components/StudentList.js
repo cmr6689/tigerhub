2d8d7bddbf1fa865bdd68ab8b24e304cb86e8f6f
Ben Radley <bdr1224@rit.edu>
2021-04-24 16:01:55 -0400

    Changed list format with funcitonality

--- src/components/StudentList.js
+++ src/components/StudentList.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import Student from "./Student";
 //import props from "props"
+import {
+    NavLink
+}from 'reactstrap';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from 'react-router-dom';
 
 
 function StudentList(props) {
     return (
         <div>
-            {props.students.map(s => <Student key={s.id} name={s.name} />)}
+            {/* {props.students.map(s => <Student key={s.id} name={s.name} />)} */}
+            <Link to='/tigerhub/advisor/student-studentsen'>
+                <NavLink href="/components/">Student Studentsen</NavLink>
+            </Link>
+            <Link to='/tigerhub/advisor/'>
+                <NavLink href="/components/">Lores Ipsum</NavLink>
+            </Link>
+            <Link to='/tigerhub/advisor/'>
+                <NavLink href="/components/">Zack Pepper</NavLink>
+            </Link>
+            <Link to='/tigerhub/advisor/'>
+                <NavLink href="/components/">Jeff Loepsk</NavLink>
+            </Link>
         </div>
     );
     
