7f0bb351a3233731aca0fd26846c62b5d84005d0
cmr6689 <cmr6689@g.rit.edu>
2021-04-25 17:19:15 -0400

    added student search

--- src/components/StudentList.js
+++ src/components/StudentList.js
@@ -2,35 +2,93 @@ import React from 'react';
 import Student from "./Student";
 //import props from "props"
 import {
+    Form, Input,
     NavLink
-}from 'reactstrap';
+} from 'reactstrap';
 import {
     BrowserRouter as Router,
     Switch,
     Route,
     Link
 } from 'react-router-dom';
+import StudentSearchContainer from "./StudentSearchContainer";
+import StudentStudentsen from "./StudentStudensen";
 
 
-function StudentList(props) {
-    return (
-        <div className="studentList">
-            {/* {props.students.map(s => <Student key={s.id} name={s.name} />)} */}
-            <Link to='/tigerhub/advisor/student-studentsen'>
-                <NavLink href="/components/">Student Studentsen</NavLink>
-            </Link>
-            <Link to='/tigerhub/advisor/'>
-                <NavLink href="/components/">Lores Ipsum</NavLink>
-            </Link>
-            <Link to='/tigerhub/advisor/'>
-                <NavLink href="/components/">Zack Pepper</NavLink>
-            </Link>
-            <Link to='/tigerhub/advisor/'>
-                <NavLink href="/components/">Jeff Loepsk</NavLink>
-            </Link>
-        </div>
-    );
-    
+export default class StudentList extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchTerm: '',
+            searchTermClicked: false,
+            students: [
+                'Student Studentsen',
+                'Lores Ipsum',
+                'Zack Pepper',
+                'Jeff Loepsk'
+            ]
+        }
+        this.showComponent = this.showComponent.bind(this);
+        this.hideComponent = this.hideComponent.bind(this);
+    }
+
+    showComponent(name) {
+        switch(name) {
+            case 'searchTermClicked':
+                this.setState({searchTermClicked: true});
+                break;
+            default:
+                this.setState(this.state);
+        }
+    }
+
+    hideComponent(name) {
+        switch(name) {
+            case 'searchTermClicked':
+                this.setState({searchTermClicked: false});
+                break;
+            default:
+                this.setState(this.state);
+        }
+    }
+
+
+    editSearchTerm = (e) => {
+        this.setState({searchTerm: e.target.value})
+    }
+
+    search = () => {
+        return this.state.students.filter(student => student.toLowerCase().includes(this.state.searchTerm.toLowerCase()));
+    }
+
+    showResults() {
+        return (
+            <StudentSearchContainer students = {this.search()} func = {this.showComponent} />
+        )
+    }
+
+    render() {
+        return (
+            <div>
+                {!this.state.searchTermClicked && (
+                    <div className="studentList">
+                        <div className='studentSearch'>
+                            <div id='search'>
+                                <Form>
+                                    <Input type='search' name='search' value={this.state.searchTerm} onChange={this.editSearchTerm} placeholder='Search for students' />
+                                </Form>
+                            </div>
+                        </div>
+                        <div className='studentSearchResults'>
+                            {this.showResults()}
+                        </div>
+                    </div>
+                )}
+                {this.state.searchTermClicked && (
+                    <StudentStudentsen />
+                )}
+            </div>
+        );
+    }
 }
-
-export default StudentList;
